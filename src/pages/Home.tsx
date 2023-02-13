1306244157ce31b7127f3c00e32fd7c50e76cd47
Taek Been Nam <taekbeen93@gmail.com>
2023-02-13 18:53:44 -0500

    chore: update react query to fetch from Youtube API on Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,31 @@
 import Card from "../components/Card/Card";
 import { useQuery } from "@tanstack/react-query";
-import { getTrendingVideos, getVideoById } from "../api/video";
+import { getTrendingVideos } from "../api/video";
 import { FormattedDate } from "react-intl";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   const { isLoading, error, data } = useQuery(
     ["most-recent"],
-    async () =>
-      fetch(`data/mostPopular.json`).then((res) => {
-        console.log("fetching");
-        return res.json();
-      }),
-    { staleTime: 5000 }
+    async () => await getTrendingVideos(),
+    {
+      staleTime: 10000,
+    }
+    // async () =>
+    //   fetch(`data/mostPopular.json`).then((res) => {
+    //     console.log("fetching");
+    //     return res.json();
+    //   }),
+    // { staleTime: 5000 }
   );
-  console.log(data);
+
   return (
     <main className="flex h-full flex-col place-items-center">
-      <section className="bg-yt-text-secondary">ChipBar</section>
+      {/* <section className="bg-yt-text-secondary">ChipBar</section> */}
       {isLoading ? (
         <p>Loading..</p>
       ) : (
-        <section className="grid w-full max-w-screen-2xl gap-4 px-14 xs:grid-cols-2 xs:px-0 sm:px-8 md:grid-cols-3 md:px-14 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        <section className="mt-8 grid w-full max-w-screen-2xl gap-4 px-14 xs:grid-cols-2 xs:px-0 sm:px-8 md:grid-cols-3 md:px-14 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           {/* TODO: add proper type check here */}
           {data.items.map(({ id, snippet }: any) => (
             <div key={id} className="grid__card h-auto overflow-hidden">
