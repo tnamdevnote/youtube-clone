4c0aa505b23b84a8f4b25fa0d06dcb914c34ea17
Taek Been Nam <taekbeen93@gmail.com>
2023-02-28 21:12:14 -0500

    feature: add toggle expander on MetaDataCard

--- src/feature/Watch/MetaDataCard.tsx
+++ src/feature/Watch/MetaDataCard.tsx
@@ -1,4 +1,5 @@
 import numeral from "numeral";
+import useBoolean from "../../hooks/useBoolean";
 
 interface MetaDataCardProps {
     viewCount: number;
@@ -6,11 +7,36 @@ interface MetaDataCardProps {
 }
 
 function DescriptionCard({ viewCount, description }: MetaDataCardProps) {
+    const { value: isMore, setTrue: showMore, toggle: toggleExpander } = useBoolean();
+
+    const handleToggle = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        toggleExpander();
+    };
+
     return (
-        <div className="h-28 cursor-pointer rounded-xl bg-yt-badge-chip-background p-3 hover:bg-yt-button-chip-background-hover">
+        <div
+            className={`${
+                isMore ? "h-fit cursor-default" : "h-28 hover:bg-yt-button-chip-background-hover"
+            } relative cursor-pointer rounded-xl bg-yt-badge-chip-background p-3 `}
+            onClick={showMore}
+        >
             <p className="text-sm font-semibold">{`${numeral(viewCount).format("0,0")} views`}</p>
-            <p className="overflow-hidden text-ellipsis whitespace-pre text-sm font-medium line-clamp-3">
+            <p
+                className={`${
+                    isMore
+                        ? "whitespace-pre-wrap"
+                        : "w-72 overflow-hidden text-ellipsis whitespace-pre-line line-clamp-3"
+                } relative text-sm font-medium `}
+            >
                 {description}
+                <button
+                    className={`${isMore ? "flex flex-col" : "absolute right-0 bottom-0 "} font-bold`}
+                    type="button"
+                    onClick={handleToggle}
+                >
+                    {isMore ? "Show less" : "Show more"}
+                </button>
             </p>
         </div>
     );
