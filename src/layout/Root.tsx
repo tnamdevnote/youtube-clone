4089871dd73b6bb0b552c37c2d9cc82346723daa
Taek Been Nam <taekbeen93@gmail.com>
2023-03-13 19:41:58 -0400

    chore: move QueryClientProvider to nearest place where API call is happening

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import PageManager from "./PageManager";
 
+const queryClient = new QueryClient();
+
 export default function Root() {
-  return (
-    <>
-      <Header />
-      <PageManager>
-        <Outlet />
-      </PageManager>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <PageManager>
+                <QueryClientProvider client={queryClient}>
+                    <Outlet />
+                </QueryClientProvider>
+            </PageManager>
+        </>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { IntlProvider } from "react-intl";
@@ -6,16 +5,12 @@ import App from "./App";
 import "./index.css";
 import { DarkModeProvider } from "./context/DarkModeContext";
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <IntlProvider locale={navigator.language}>
-                <DarkModeProvider>
-                    <App />
-                </DarkModeProvider>
-            </IntlProvider>
-        </QueryClientProvider>
+        <IntlProvider locale={navigator.language}>
+            <DarkModeProvider>
+                <App />
+            </DarkModeProvider>
+        </IntlProvider>
     </React.StrictMode>
 );
